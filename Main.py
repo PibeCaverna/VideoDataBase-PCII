45df726d12490e1a4d8a9d303f9a2133933d6f8d
Franco Aniello Falconieri <francoafalconieri@gmail.com>
2024-11-14 16:45:49 -0300

    Agregada data para meterse en la database

---
+++ Main.py
@@ -0,0 +1,11 @@
+import mysql.connector
+config = {
+  "user": "dilfar_user",
+  "password": "2024CtrlAltDelete",
+  "host": "luca.ar",
+  "database": "dilfar_VideoDB-PCII"
+}
+
+cnx = mysql.connector.connect(**config)
+print ("Conectado: ", cnx.is_connected())
+cnx.close()
