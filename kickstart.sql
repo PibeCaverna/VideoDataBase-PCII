a003214c2dde0f907b815739978ef431eb116e1a
Luca <krombergerluca@gmail.com>
2024-11-19 15:43:48 -0300

    es ER no UML -- Mas tablas

--- kickstart.sql
+++ kickstart.sql
@@ -1,21 +1,21 @@
 CREATE TABLE Usuarios(
 	id_usuario INT PRIMARY KEY AUTO_INCREMENT,
-	e_mail VARCHAR(20) NOT NULL,
+	e_mail VARCHAR(20) UNIQUE NOT NULL,
 	password VARCHAR(20) NOT NULL
 )
 #
 CREATE TABLE Autenticaciones(
 	id_auth INT PRIMARY KEY AUTO_INCREMENT,
-	id_usuario INT,
+	id_usuario INT NOT NULL,
 	success BOOLEAN NOT NULL,
-	momento TIMESTAMP, 
+	momento TIMESTAMP NOT NULL, 
 	
 	FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario)
 )
 #
 CREATE TABLE Perfiles(
 	id_peril INT PRIMARY KEY AUTO_INCREMENT,
-	id_usuario INT,
+	id_usuario INT NOT NULL,
 	es_infante BOOLEAN NOT NULL,
 	
 	FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario)
@@ -23,7 +23,64 @@ CREATE TABLE Perfiles(
 #
 CREATE TABLE Artistas(
 	id_artista INT PRIMARY KEY AUTO_INCREMENT,
-	nombre VARCHAR(20),
-	apellido VARCHAR(20),
+	nombre VARCHAR(20) NOT NULL,
+	apellido VARCHAR(20) NOT NULL,
 	pseudonimo VARCHAR(20)
+)
+#
+CREATE TABLE Videos(
+	id_video INT PRIMARY_KEY AUTO_INCREMENT,
+	ubicacion VARCHAR,
+	nombre VARCHAR(20) NOT NULL,
+	descripcion VARCHAR(200)
+)
+#
+CREATE TABLE Series(
+	id_serie INT PRIMARY_KEY AUTO_INCREMENT,
+	nombre VARCHAR(20) NOT NULL,
+	descripcion VARCHAR(200)
+)
+#
+CREATE TABLE Capitulos(
+	id_capitulo INT PRIMARY_KEY,
+	id_serie INT NOT NULL,
+	temporada INT,
+
+	FOREIGN KEY (id_capitulo) REFERENCES Videos(id_video),
+	FOREIGN KEY (id_serie) REFERENCES Series(id_serie)
+)
+#
+CREATE TABLE Sagas(
+	id_saga INT PRIMARY_KEY AUTO_INCREMENT,
+	nombre VARCHAR(20) NOT NULL,
+	descripcion VARCHAR(200)
+)
+#
+CREATE TABLE Peliculas(
+	id_pelicula INT PRIMARY_KEY,
+	id_saga INT,
+
+	FOREIGN KEY (id_pelicula) REFERENCES Videos(id_video),
+	FOREIGN KEY (id_saga) REFERENCES Sagas(id_saga)
+)
+#
+CREATE TABLE Progresos(
+	id_perfil INT,
+	id_video INT,
+	tiempo_progreso TIME,
+
+	PRIMARY KEY id_usuario, id_video,
+	FOREIGN KEY (id_perfil) REFERENCES Perfiles(id_perfil),
+	FOREIGN KEY (id_video) REFERENCES Videos(id_video)
+)
+#
+CREATE TABLE Creditos(
+	id_artista INT NOT NULL,
+	id_video INT NOT NULL,
+	rol SET("Actor","Director","Productor"),
+	nombre_personaje VARCHAR(20),
+	
+	PRIMARY KEY (id_artista, id_video),
+	FOREIGN KEY (id_artista) REFERENCES Artistas(id_artista),
+	FOREIGN KEY (id_video) REFERENCES Videos(id_video)
 )
