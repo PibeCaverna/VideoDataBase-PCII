38f9394647b00a7b53c7abb41e5e86db671ed90d
Franco Aniello Falconieri <francoafalconieri@gmail.com>
2024-11-21 09:36:44 -0300

    solucionado el kickstart. Hay que ver si la excepción es una forma sensata de chequear

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
-from settings import all
+from settings import *
 import mysql.connector
 from utils.kickstart import kickstart
 
 conexion = mysql.connector.connect(**DBCREDENTIALS)
 print ("Conectado: ", conexion.is_connected())
+ecatch = kickstart(conexion)
+
 conexion.close()
 
--- utils/kickstart.py
+++ utils/kickstart.py
@@ -1,12 +1,31 @@
 import mysql.connector
+from mysql.connector.errors import ProgrammingError
 
 def kickstart(conexion):
+    ''' Inicia todas las tablas de kickstart.sql, en caso de que no estén iniciadas.
+    Retorna una lista ordenada de booleanos, donde falso indica una query que no se realizo.
+    NO SE RECOMIENDA SU USO PARA OTRO TIPO DE QUERY, PUEDE RESULTAR EN CASOS INESPERADOS.'''
     startup = ""
     kickfile = open("kickstart.sql","r")
+    sub1 = "CREATE TABLE "
+    sub2 = "("
+    #Agarra todas las querys de kickstart.sql y las mete en un string
     for line in kickfile:
         startup += (line)
     kickfile.close()
+    #Separa todas las querys en una lista. Es importante agregar un # entre cada query para poder separarlas
     startup = startup.split("#")
     with conexion.cursor() as cursor:
+        ecatch = []
         for query in startup:
-            cursor.execute(query)    
+             #le mandamos excepción para buscar un programming error, no se si es demasiado general
+             #o si puede haber una solución más elegante
+             try:
+                cursor.execute(query)
+             except ProgrammingError:
+                ecatch.append(False)
+             else:
+                ecatch.append(True)
+    return ecatch 
+    #ecatch indica con False las tablas que no se crearon y con true las que se crearon
+             
