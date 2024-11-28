dd95913dbbd2608b31450f77a7e98440fb709071
LK PC <krombergerluca@gmail.com>
2024-11-27 23:19:01 -0300

    Funcionan ultimos vistos

--- utils/secciones.py
+++ utils/secciones.py
@@ -7,7 +7,6 @@ def Sections(id_profile,conexion):
     pelis_formateadas = [f"[{id_peli}]-{get_pelis_name([id_peli], conexion)[0]}" for id_peli in pelis_no_terminadas]
     series_formateadas = [f"[{id_serie}]-{get_series_name([id_serie], conexion)[0]}" for id_serie in series_no_terminadas]
 
-    print(pelis_formateadas)
     return pelis_formateadas, series_formateadas
 
 
@@ -15,46 +14,46 @@ def Sections(id_profile,conexion):
 def Get_videos_no_finalizados(id_profile,conexion):
 
     #obtengo todas las peliculas sin terminar
-    consulta_pelis = """Select Distinct V.id_video 
-                        From Videos V, Perfiles Pe, Peliculas M, Progresos Pr  
-                        Where %s = Pr.id_perfil and Pr.id_video = V.id_video and V.id_video = M.id_video and Pr.progreso < 100 and Pr.progreso > 0"""
+    consulta_pelis = """Select V.id_video 
+                        from Videos V natural join Peliculas M natural join Progresos Pr natural join Perfiles Pe
+                        Where Pe.id_perfil = %d and Pr.progreso < 100 and Pr.progreso > 0"""
     #obtengo todas las series de las que todabia falta terminar algun capitulo
-    consulta_cap_por_terminar = """Select Distinct S.id_serie
-                                    From Videos V, Perfiles Pe, Capitulos C, Progresos Pr, Series S  
-                                    Where %s = Pr.id_perfil and Pr.id_video = V.id_video and V.id_video = C.id_video and C.id_serie = S.id_serie and Pr.progreso < 100 and Pr.progreso > 0"""
+    consulta_cap_por_terminar = """Select S.id_serie
+                                    From Videos V natural join Perfiles Pe natural join Capitulos C natural join Progresos Pr natural join Series S  
+                                    Where %d = Pe.id_perfil and Pr.progreso < 100 and Pr.progreso > 0"""
     #devuelve las id de todas las series vistas
     consulta_serie_por_terminar = """Select Distinct S.id_serie
-                  From Videos V, Perfiles Pe, Capitulos C, Progresos Pr, Series S  
-                  Where %s = Pr.id_perfil and Pr.id_video = V.id_video and V.id_video = C.id_video and C.id_serie = S.id_serie"""
+                  From Videos V natural join Perfiles Pe natural join Capitulos C natural join Progresos Pr natural join Series S  
+                  Where %d = Pe.id_perfil"""
     #devuelve el progreso del ultimo capitulo de una serie dada
-    consulta_ultimo_cap = """Select Pr.progreso 
-                            From Videos V, Perfiles Pe, Capitulos C, Progresos Pr, Series S  
-                            Where %s = Pr.id_perfil and Pr.id_video = V.id_video and V.id_video = C.id_video and C.id_serie = S.id_serie and C.id_serie = %s
-                            Order by C.temporada DES, C.num_capitulo DES
+    consulta_ultimo_cap = """Select Pr.progreso, C.temporada, C.num_capitulo
+                            From Videos V natural join Perfiles Pe natural join Capitulos C natural join Progresos Pr natural join Series S  
+                            Where %d = Pe.id_perfil and C.id_serie = %d
+                            Order by C.temporada DESC, C.num_capitulo DESC
                             Limit 1
                             """
                              
 
     
     with conexion.cursor() as cursor:
-        cursor.execute(consulta_pelis , (id_profile,))
+        cursor.execute(consulta_pelis % (id_profile))
         pelis = cursor.fetchall()
 
-        cursor.execute(consulta_cap_por_terminar , (id_profile,))
+        cursor.execute(consulta_cap_por_terminar % (id_profile))
         caps = cursor.fetchall()
 
-        cursor.execute(consulta_serie_por_terminar , (id_profile,))
+        cursor.execute(consulta_serie_por_terminar % (id_profile))
         series_vistas = cursor.fetchall()
 
         series_por_terminar = []
         for x in series_vistas: #para cada serie que fue vista, se fija si el ultimo capitulo de esta fue terminado
-            cursor.execute(consulta_ultimo_cap , (id_profile, x))
+            cursor.execute(consulta_ultimo_cap % (id_profile, x[0]))
             last_cap = cursor.fetchall()
-
             if last_cap != 100 and x not in caps: #si el ultimo cap de una serie vista no esta terminado guarda el id de la serie si
                                                   #este no esta ya guardad en la lista de caps
-                series_por_terminar.append(x)
-
+                series_por_terminar.append(x[0])
+        print(caps)
+        print(pelis)
         return (pelis, caps + series_por_terminar)
 
 
